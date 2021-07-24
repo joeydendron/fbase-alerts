05b01eccf35d919e17d776bacde2259917260b57
joeydendron <joey.dendron@gmail.com>
2021-07-25 00:18:16 +0100

    Added singleton constructor in Alerter, .gitignored config/ directory
    config/ directory is for prototyping code, to store developer Firebase credentials...

--- src/JoeyDendron/FbaseAlerts/Alerter.php
+++ src/JoeyDendron/FbaseAlerts/Alerter.php
@@ -1,14 +1,21 @@
 <?php
 namespace JoeyDendron\FbaseAlerts;
-use Kreait\Firebase\Factory;
+use Kreait\Firebase\Factory, Kreait\Firebase\Database;
 
 class Alerter {
-    public static function instance(array $config) {
-        return new Alerter($config);
+    private static $instance = null;
+    private $db;
+
+    public static function instance($pathToFBCredentials, $dbUrl) {
+        if(self::$instance == null) {
+            $db = (new Factory)->withServiceAccount($pathToFBCredentials)->withDatabaseUri($dbUrl)->createDatabase();
+            self::$instance = new Alerter($db);
+        }
+        return self::$instance;
     }
 
-    public function __construct(array $config) {
-
+    private function __construct(Database $db) {
+        $this->db = $db;
     }
 
     public function alert($subject, $body) {
