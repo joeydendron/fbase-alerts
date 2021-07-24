1954a9872e2d537c8bbe3443bd743252c07d0a1c
joeydendron <joey.dendron@gmail.com>
2021-07-24 19:52:52 +0100

    Moved index.php, added static instance method to Alerter

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require(__DIR__.'/vendor/autoload.php');
-
-use JoeyDendron\FbaseAlerts\Alerter;
-
-(new Alerter)->alert("Subject", "Body");
--- src/JoeyDendron/FbaseAlerts/Alerter.php
+++ src/JoeyDendron/FbaseAlerts/Alerter.php
@@ -1,7 +1,16 @@
 <?php
 namespace JoeyDendron\FbaseAlerts;
+use Kreait\Firebase\Factory;
 
 class Alerter {
+    public static function instance(array $config) {
+        return new Alerter($config);
+    }
+
+    public function __construct(array $config) {
+
+    }
+
     public function alert($subject, $body) {
         exit("Alert!");
     }
